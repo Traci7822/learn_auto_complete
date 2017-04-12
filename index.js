6c21df1fb0271b7554af232ec4261aec8031c86a
Traci7822 <Tracir@gmail.com>
2017-04-12 10:23:45 -0600

    new div showing fields

--- index.js
+++ index.js
@@ -6,18 +6,87 @@
 // // return focus to input to be able to send w/ a
 // // toggle clears input
 //
-// var save_image_url = 'https://c1.staticflickr.com/3/2900/33745640515_a90c44b434_t.jpg';
+var save_image_url = 'https://c1.staticflickr.com/3/2900/33745640515_a90c44b434_t.jpg';
 // var content_image_url = 'https://c1.staticflickr.com/4/3943/33793771665_e25336d636_t.jpg';
-// var remove_image_url = 'https://c1.staticflickr.com/4/3684/33135618604_414134ce5c_t.jpg';
+var remove_image_url = 'https://c1.staticflickr.com/4/3684/33135618604_414134ce5c_t.jpg';
 // var contentCollection = [];
 // var filteredList = [];
 //
   window.onload = function(){
+    buildExtensionField();
+    buildContentActions();
+  }
+
+  function buildExtensionField() {
     var expertSection = document.getElementsByClassName('module__foot')[0];
     var newSection = document.createElement('div');
     newSection.setAttribute('class', 'content-filler');
     expertSection.children[1].before(newSection);
+  }
+
+  function getExtensionField() {
+    return document.getElementsByClassName('module__foot')[0].children[1];
+  }
+
+  function buildContentActions() {
+    var elements = [];
+    elements.push(createButton('save'));
+    elements.push(buildMenuField());
+    elements.push(createButton('remove'));
+    appendButtonsToExtension(elements);
+  }
+
+  function createButton(type) {
+    var newButton = document.createElement('button');
+    var image = buttonImage(type);
+    newButton.appendChild(image);
+    newButton.id = `${type}_button`;
+    newButton.style = "cursor: pointer";
+    var titleName = type.charAt(0).toUpperCase() + type.slice(1);
+    newButton.setAttribute('title', `${titleName} content`);
+    newButton.onclick = function(event){
+      var type = event.target.id.split("_")[0];
+      eval(type + 'Action()');
     }
+    return newButton;
+ }
+
+ function buttonImage(type){
+    var image = document.createElement('img');
+    var image_url = `${type}_image_url`;
+    image.id = `${type}_image`;
+    image.src = eval(image_url);
+    return image;
+ }
+
+ function appendElementsToExtension(buttons) {
+   var section = getExtensionField();
+   for (i in buttons) {
+     section.appendChild(buttons[i]);
+   }
+ }
+
+ function buildMenuField() {
+   var menuElement = document.createElement('select');
+   menuElement.setAttribute('id', 'menu_element');
+   return menuElement;
+ }
+
+ // function createButton(type){
+ //   var button = document.createElement('button');
+ //   var image = createImage(type);
+ //   button.appendChild(image);
+ //   button.id = `${type}_button`;
+ //   button.style = "cursor: pointer";
+ //   var titleName = type.charAt(0).toUpperCase() + type.slice(1);
+ //   button.setAttribute('title', `${titleName} content`);
+ //   button.onclick = function(event){
+ //     var type = event.target.id.split("_")[0];
+ //     eval(type + 'Action()');
+ //   }
+ //   return button;
+ // }
+
 //   // event.preventDefault;
 //   loadExtension();
 //   activeChats();
